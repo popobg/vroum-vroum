8c33d1c936551cbd778429d9d25489192602ffb1
popobg <pauline.bouyssou1@gmail.com>
2025-05-12 10:35:00 +0200

    création projet back

---
+++ back/vroum_vroum/src/main/java/demo/vroum_vroum/VroumVroumApplication.java
@@ -0,0 +1,13 @@
+package demo.vroum_vroum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VroumVroumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VroumVroumApplication.class, args);
+    }
+
+}
---
+++ back/vroum_vroum/src/test/java/demo/vroum_vroum/VroumVroumApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.vroum_vroum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VroumVroumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
