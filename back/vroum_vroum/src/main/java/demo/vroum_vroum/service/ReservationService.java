3f1dcf4d3e66084122ca23e42e7bd8bddce6b24b
Maxence <maxence.ogier@gmail.com>
2025-05-13 16:40:20 +0200

    vehicule service  et reservation

--- back/vroum_vroum/src/main/java/demo/vroum_vroum/service/ReservationService.java
+++ back/vroum_vroum/src/main/java/demo/vroum_vroum/service/ReservationService.java
@@ -1,5 +1,7 @@
 package demo.vroum_vroum.service;
 
+import demo.vroum_vroum.entity.Reservation;
+import demo.vroum_vroum.exeption.Controle;
 import demo.vroum_vroum.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,30 @@ public class ReservationService {
 
     @Autowired
     ReservationRepository reservationRepository;
+
+    public Iterable<Reservation> findAll() {return reservationRepository.findAll();}
+
+    public Reservation findById(int id) throws Controle {
+        if (reservationRepository.findById(id).isPresent()) {
+            return reservationRepository.findById(id).get();
+        } else {
+            throw new Controle("L'id n'existe pas");
+        }
+    }
+
+    public void create(Reservation reservation) throws Controle {
+        reservationRepository.save(reservation);
+    }
+
+    public void update(Reservation reservation) throws Controle {
+        reservationRepository.save(reservation);
+    }
+
+    public void delete(int id) throws Controle {
+        if (reservationRepository.findById(id).isPresent()) {
+            reservationRepository.deleteById(id);
+        } else {
+            throw new Controle("L'id n'existe pas");
+        }
+    }
 }
