d0548a4d448d85687ea0b9203bdab9a05292ab41
johan guillen <sioupes34@gmail.com>
2025-05-14 08:46:16 +0200

    reservation service

---
+++ back/vroum_vroum/src/main/java/demo/vroum_vroum/service/ReservationCovoit.java
@@ -0,0 +1,43 @@
+package demo.vroum_vroum.service;
+
+import demo.vroum_vroum.entity.Reservation;
+import demo.vroum_vroum.exeption.Controle;
+import demo.vroum_vroum.repository.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationService {
+
+    @Autowired
+    ReservationRepository reservationRepository;
+
+    public Iterable<Reservation> findByCollaborateur(Collaborateur) throws Controle {
+        if(reservationRepository.findByCollaborateur(Collaborateur).isPresent())
+        return reservationRepository.findByCollaborateur();
+    }
+
+    public Reservation findById(int id) throws Controle {
+        if (reservationRepository.findById(id).isPresent()) {
+            return reservationRepository.findById(id).get();
+        } else {
+            throw new Controle("L'id n'existe pas");
+        }
+    }
+
+    public void create(Reservation reservation) throws Controle {
+        reservationRepository.save(reservation);
+    }
+
+    public void update(Reservation reservation) throws Controle {
+        reservationRepository.save(reservation);
+    }
+
+    public void delete(int id) throws Controle {
+        if (reservationRepository.findById(id).isPresent()) {
+            reservationRepository.deleteById(id);
+        } else {
+            throw new Controle("L'id n'existe pas");
+        }
+    }
+}
