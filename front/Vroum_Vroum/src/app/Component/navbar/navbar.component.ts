52d03a31610325f19b32e66652e8a3b4aab4b1c4
Pauline Bouyssou <pauline.bouyssou1@gmail.com>
2025-11-04 23:19:04 +0100

    Fonctionnement navbar modifié

--- front/Vroum_Vroum/src/app/Component/navbar/navbar.component.ts
+++ front/Vroum_Vroum/src/app/Component/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../core/auth/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -9,4 +11,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  isAuthenticated$: Observable<boolean>;
+
+  constructor(private authService: AuthService) {
+    this.isAuthenticated$ = this.authService.isAuthenticated$;
+  }
+
+  logout() {
+    this.authService.logout().subscribe();
+  }
+
+}
