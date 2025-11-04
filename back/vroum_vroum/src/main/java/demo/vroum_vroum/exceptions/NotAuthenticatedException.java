f34d4f0b38d4a723bd12d3110f14e253b65c534d
Pauline Bouyssou <pauline.bouyssou1@gmail.com>
2025-11-04 10:18:16 +0100

    Renommage erreur NotAuthenticatedException

--- back/vroum_vroum/src/main/java/demo/vroum_vroum/exceptions/NotAuthenticatedException.java
+++ back/vroum_vroum/src/main/java/demo/vroum_vroum/exceptions/NotAuthenticatedException.java
@@ -3,9 +3,9 @@ package demo.vroum_vroum.exceptions;
 /**
  * Classe d'exception personnalisée si l'application ne trouve pas l'utilisateur connecté
  */
-public class UserNotFoundException extends RuntimeException {
+public class NotAuthenticatedException extends RuntimeException {
     /** Constructeur sans argument */
-    public UserNotFoundException() {
+    public NotAuthenticatedException() {
         super("Utilisateur non connecté");
     }
 
@@ -13,7 +13,7 @@ public class UserNotFoundException extends RuntimeException {
      * Constructeur
      * @param message message d'erreur personnalisé
      */
-    public UserNotFoundException(String message) {
+    public NotAuthenticatedException(String message) {
         super(message);
     }
 }
