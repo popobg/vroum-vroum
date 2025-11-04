d0bc17b076f7796168019633bf4c2c230ebbe262
Pauline Bouyssou <pauline.bouyssou1@gmail.com>
2025-11-04 23:22:43 +0100

    Fonctionnement home modifié

--- front/Vroum_Vroum/src/app/Component/pages/home/home.component.ts
+++ front/Vroum_Vroum/src/app/Component/pages/home/home.component.ts
@@ -2,27 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { UserService, Collaborateur } from '../../../../core/auth/user.service';
-import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../../../../core/auth/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, HttpClientModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   collaborateur?: Collaborateur;
   errorMessage = '';
+  // ! = dit à JS que la propriété sera initialisée avant d'être utilisée
+  isAuthenticated$!: Observable<boolean>;
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private authService: AuthService) {
+  }
 
   ngOnInit() {
-    const pseudo = localStorage.getItem('pseudo');
-    const password = localStorage.getItem('password');
-    if (!pseudo || !password) return;
+    // Propriété indiquant si l'utilisateur est connecté ou non
+    this.isAuthenticated$ = this.authService.isAuthenticated$;
 
-    this.userService.getCurrentUser(pseudo!, password!).subscribe({
+    if (!this.isAuthenticated$) return;
+
+    this.userService.getProfile().subscribe({
       next: (user) => this.collaborateur = user,
       error: (err) => console.error('Non authentifié', err)
     });
