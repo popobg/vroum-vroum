77fa0bdaad1c96bdf87cc13638002f0304835c16
Pauline Bouyssou <pauline.bouyssou1@gmail.com>
2025-11-04 23:17:58 +0100

    Mise en forme routes

--- front/Vroum_Vroum/src/app/app.routes.ts
+++ front/Vroum_Vroum/src/app/app.routes.ts
@@ -2,14 +2,25 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './Component/pages/login/login.component';
 import { HomeComponent } from './Component/pages/home/home.component';
 import { ReservationCovoitComponent } from './Component/pages/reservation.covoit.component/reservation.covoit.component'
+
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
   {
     path: 'rechercher-covoit',
     loadComponent: () => import("./Component/pages/recherche_covoit/recherche.covoit.component").then(m => m.RechercheCovoitComponent)
   },
   { path: 'mes-reservations', component: ReservationCovoitComponent },
   // { path: 'mes-covoits-organises', component: MesCovoitsOrganisesComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  {
+    path: '**',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
 ];
