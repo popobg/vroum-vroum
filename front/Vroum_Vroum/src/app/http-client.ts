be2d3fca7d254fdaa470d387f54dfd1f10c2d22d
Pauline Bouyssou <pauline.bouyssou1@gmail.com>
2025-11-06 15:16:55 +0100

    Modif classe HttpClient custom

--- front/Vroum_Vroum/src/app/http-client.ts
+++ front/Vroum_Vroum/src/app/http-client.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 /**
@@ -21,19 +21,16 @@ export class MyHttpClient {
      * @returns la ressource demandée ou une erreur
      */
     get(url: string, parameters?: any): any {
-        if (!parameters) {
-            return this.http.get(`${this.baseUrl}${url}`, { withCredentials: true });
-        }
         return this.http.get(`${this.baseUrl}${url}`, { params: parameters, withCredentials: true });
     }
 
     /**
      * Méthode permettant d'envoyer une requête sécurisée au backend pour créer une ressource.
      * @param url url de la requête
-     * @param data corps de requête (peut être un objet vide)
-     * @returns retour de la requête (peut être vide - code 204)
+     * @param data corps de requête (optionnel)
+     * @returns retour de la requête (peut être void - code 204)
      */
-    post(url: string, data: any): any {
+    post(url: string, data?: any): Observable<any> {
         return this.csrfRequestWrapper((token: string) => {
             return this.http.post(
                 `${this.baseUrl}${url}`,
@@ -52,10 +49,10 @@ export class MyHttpClient {
     /**
      * Méthode permettant d'envoyer une requête sécurisée au backend pour modifier une ressource existante.
      * @param url url de la requête
-     * @param data corps de requête (peut être un objet vide)
-     * @returns retour de la requête (peut être vide - code 204)
+     * @param data corps de requête (optionnel)
+     * @returns retour de la requête (peut être void - code 204)
      */
-    put(url: string, data: any): any {
+    put(url: string, data?: any): Observable<any> {
         return this.csrfRequestWrapper((token: string) => {
             return this.http.put(
                 `${this.baseUrl}${url}`,
@@ -74,15 +71,14 @@ export class MyHttpClient {
     /**
      * Méthode permettant d'envoyer une requête sécurisée au backend pour supprimer une ressource existante.
      * @param url url de la requête
-     * @returns retour de la requête (peut être vide - code 204)
+     * @returns retour de la requête (peut être void - code 204)
      */
-    delete(url: string): any {
+    delete(url: string): Observable<any> {
         return this.csrfRequestWrapper((token: string) => {
             return this.http.delete(
                 `${this.baseUrl}${url}`,
                 {
                     headers: new HttpHeaders({
-                        "Content-Type": "application/x-www-form-urlencoded",
                         "X-CSRF-TOKEN": token
                     }),
                     withCredentials: true
@@ -92,11 +88,12 @@ export class MyHttpClient {
     }
 
     /**
-     * Wrapper appelant la fonction reqWithCsrfToken censée exécuter une requête avec le token fourni.
-     * @returns Observable retourné par reqWithCsrfToken
+     * Wrapper adressant une requête au serveur pour récupérer le token CSRF puis
+     * appelant le callback reqWithCsrfToken exécutant une requête avec le token fourni.
+     * @returns Observable du type de la ressource retournée par reqWithCsrfToken (peut être void - code http 204)
      */
-    private csrfRequestWrapper(reqWithCsrfToken: (csrfToken: string) => Observable<Object>) {
-        return new Observable((subscriber) => {
+    private csrfRequestWrapper(reqWithCsrfToken: (csrfToken: string) => Observable<any>) {
+        return new Observable<any>((subscriber) => {
             this.http.get(`${this.baseUrl}/csrf/token`, { withCredentials: true }).subscribe((body: any) => {
                 reqWithCsrfToken(body.token).subscribe({
                     next: (data) => subscriber.next(data),
