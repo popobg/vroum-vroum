21c38c7aa366f4ff6571f61ab0088b44c780789b
Maxence <maxence.ogier@gmail>
2025-11-07 10:00:48 +0100

    vehicule service

--- front/Vroum_Vroum/src/app/app.routes.ts
+++ front/Vroum_Vroum/src/app/app.routes.ts
@@ -1,15 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Component/pages/login/login.component';
 import { HomeComponent } from './Component/pages/home/home.component';
-import { ReservationCovoitComponent } from './Component/pages/reservation.covoit.component/reservation.covoit.component'
+import { Reservation_covoitComponent } from './Component/pages/reservation_covoit.component/reservation_covoit.component';
+import { CovoitOrganises } from './Component/pages/covoit-organises/covoit-organises'
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   {
     path: 'rechercher-covoit',
-    loadComponent: () => import("./Component/pages/recherche_covoit/recherche.covoit.component").then(m => m.RechercheCovoitComponent)
+    loadComponent: () => import("./Component/pages/recherche_covoit/recherche_covoit.component").then(m => m.Recherche_covoitComponent)
   },
-  { path: 'mes-reservations', component: ReservationCovoitComponent },
-  // { path: 'mes-covoits-organises', component: MesCovoitsOrganisesComponent },
+  { path: 'mes-reservations', component: Reservation_covoitComponent },
+  { path: 'mes-covoits-organises', component: CovoitOrganises },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
