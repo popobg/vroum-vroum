fd60d4472b9b37934dabdc11544f47c5bb6677a6
Pauline Bouyssou <pauline.bouyssou1@gmail.com>
2025-11-18 18:21:19 +0100

    Renommage constantes conseils Sonarqube

--- back/vroum_vroum/src/main/java/demo/vroum_vroum/utils/ErrorMessages.java
+++ back/vroum_vroum/src/main/java/demo/vroum_vroum/utils/ErrorMessages.java
@@ -1,20 +1,20 @@
 package demo.vroum_vroum.utils;
 
 /**
- * Enumération pour les messages d'erreur
+ * Classe utilitaire de constantes de messages d'erreur.
  */
 public final class ErrorMessages {
     private ErrorMessages() {
         throw new IllegalStateException("Classe utilitaire de constantes");
     }
 
-    public static final String ERROR_MESSAGE_PASSWORD = "Le mot de passe doit contenir au moins 8 caractères, dont au minimum une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial";
-    public static final String ERROR_MESSAGE_UNKNOWN_ID = "Pas de collaborateur trouvé pour l'ID ";
-    public static final String ERROR_MESSAGE_UNKNOWN_USERNAME = "Pas d'utilisateur trouvé pour le pseudo ";
-    public static final String ERROR_MESSAGE_NO_CONNECTED_USER = "Pas d'utilisateur connecté";
-    public static final String ERROR_MESSAGE_ID_FOR_NEW_ITEM = "Un nouveau collaborateur ne peut pas avoir d'ID";
-    public static final String ERROR_MESSAGE_PSEUDO = "Le collaborateur doit avoir un pseudo";
-    public static final String ERROR_MESSAGE_NOM = "Le nom doit comporter au moins deux caractères";
-    public static final String ERROR_MESSAGE_PRENOM = "Le prénom doit comporter au moins deux caractères";
-    public static final String ERROR_MESSAGE_EMAIL = "Le format de l'email n'est pas valide";
+    public static final String INVALID_PASSWORD_ERROR_MESSAGE = "Le mot de passe doit contenir au moins 8 caractères, dont au minimum une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial";
+    public static final String UNKNOWN_ID_ERROR_MESSAGE = "Pas de collaborateur trouvé pour l'ID donné";
+    public static final String UNKNOWN_USERNAME_ERROR_MESSAGE = "Pas d'utilisateur trouvé pour le pseudo donné";
+    public static final String NO_CONNECTED_USER_ERROR_MESSAGE = "Pas d'utilisateur connecté";
+    public static final String ID_FOR_NEW_ITEM_ERROR_MESSAGE = "Un nouveau collaborateur ne peut pas avoir d'ID";
+    public static final String INVALID_PSEUDO_ERROR_MESSAGE = "Le collaborateur doit avoir un pseudo";
+    public static final String INVALID_NOM_ERROR_MESSAGE = "Le nom doit comporter au moins deux caractères";
+    public static final String INVALID_PRENOM_ERROR_MESSAGE = "Le prénom doit comporter au moins deux caractères";
+    public static final String INVALID_EMAILE_ERROR_MESSAGE = "Le format de l'email n'est pas valide";
 }
